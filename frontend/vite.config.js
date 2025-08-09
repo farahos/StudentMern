6d27ae201c260b6f93a31f002d75c5db7b2b630b
Farah <farah@example.com>
2025-08-09 21:28:35 +0300

    upted news

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
      tailwindcss()],
      server :{
     proxy:{
-      '/api': "http://localhost:8000"
+      '/api': "https://studentmern.onrender.com/"
     }
   }
 })
