f78c1682e0395acf440c356ded9c1819d4739c66
Farah <farah@example.com>
2025-08-09 22:41:05 +0300

    updated cors news aiom

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import cors from 'cors';
 const app = express();
 const PORT = 8000
 app.use(cors({
-  origin: 'https://schoolsys-phcs.onrender.com/',
+  origin: 'https://schoolsys-aiom.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
