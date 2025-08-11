745e4dfc347da96fc7e30aeff8686ed067f82a32
Farah <farah@example.com>
2025-08-11 22:28:39 +0300

    updated 1.4 named

--- Full Stack/backend/config/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-export const port = process.env.PORT;
-export const dburl = process.env.mongo_url;
-export const jwt_secret = process.env.jwt_secret;
