716245faf5160c23798c1a174fde30f79e3b8426
Farah <farah@example.com>
2025-08-22 18:09:46 +0300

    New vertion 2025 up 1.4

---
+++ backend/routes/billRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getBills, markAsPaid } from "../controller/billController.js";
+
+const router = express.Router();
+
+router.get("/", getBills);
+router.put("/:id/pay", markAsPaid);
+
+export default router;
+
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import userRouter from './routes/UserRoute.js';
 import studentRouter from './routes/StudentRoute.js';
 import cors from 'cors';
 import attendanceRouter from './routes/attendanceRouter.js';
+import Bill from './model/Bill.js';
+import billRoutes from './routes/billRoutes.js';
 
 
 
@@ -16,10 +18,25 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
+// 📌 Cron Job: one month
+nodeCron.schedule("0 0 1 * *", async () => {
+  const oneMonthAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000); // 30 days ago
+  try {
+    const bills = await Bill.find({ status: "Paid", lastPaidAt: { $lte: oneMonthAgo } });
+    for (let bill of bills) {
+      bill.status = "Unpaid";
+      await bill.save();
+      console.log(`🔄 Bill ${bill._id} reverted back to Unpaid after one month`);
+    }
+  } catch (error) {
+    console.error("Error in cron job:", error);
+  }
+});
 app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/student', studentRouter);
 app.use('/api/attendance', attendanceRouter);
+app.use('/api/bills', billRoutes);
 
 conectBD();
 app.listen(PORT ,()=>{
