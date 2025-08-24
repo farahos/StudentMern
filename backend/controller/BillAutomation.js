798b9ea83b5a29a4a8f247db25ef92097e2544e8
Your Name <your@email.com>
2025-08-24 15:26:21 +0300

    update cusub

--- backend/controller/BillAutomation.js
+++ backend/controller/BillAutomation.js
@@ -1,40 +1,39 @@
-import cron from "node-cron";
-import student from "../model/Student.js";
-import Bill from "../model/Bill.js";
+// import cron from "node-cron";
+// import student from "../model/Student.js";
+// import Bill from "../model/Bill.js";
 
-import "./controller/BillAutomation.js";
 
-// Jadwal: 1-da bil kasta 12:00 AM
-cron.schedule("* * * * *", async () => {
+// // Jadwal: 1-da bil kasta 12:00 AM
+// cron.schedule("* * * * *", async () => {
 
-  try {
-    console.log("🔄 Running monthly bill generation job...");
+//   try {
+//     console.log("🔄 Running monthly bill generation job...");
 
-    const students = await student.find();
-    const currentMonth = new Date().toISOString().slice(0, 7); // YYYY-MM
+//     const students = await student.find();
+//     const currentMonth = new Date().toISOString().slice(0, 7); // YYYY-MM
 
-    for (const stud of students) {
-      // Check haddii bill horey loo sameeyay bishaas
-      const existingBill = await Bill.findOne({
-        student: stud._id,
-        month: currentMonth,
-      });
-      if (!existingBill) {
-        await Bill.create({
-          student: stud._id,
-          month: currentMonth,
-          amount: stud.fee,
-          status: "Unpaid",
-        });
+//     for (const stud of students) {
+//       // Check haddii bill horey loo sameeyay bishaas
+//       const existingBill = await Bill.findOne({
+//         student: stud._id,
+//         month: currentMonth,
+//       });
+//       if (!existingBill) {
+//         await Bill.create({
+//           student: stud._id,
+//           month: currentMonth,
+//           amount: stud.fee,
+//           status: "Unpaid",
+//         });
 
-        console.log(`✅ Bill created for ${stud.studentName} (${currentMonth})`);
-      } else {
-        console.log(`⚠️ Bill already exists for ${stud.studentName} (${currentMonth})`);
-      }
-    }
+//         console.log(`✅ Bill created for ${stud.studentName} (${currentMonth})`);
+//       } else {
+//         console.log(`⚠️ Bill already exists for ${stud.studentName} (${currentMonth})`);
+//       }
+//     }
 
-    console.log("🎉 Monthly bills generation completed!");
-  } catch (error) {
-    console.error("❌ Error in bill generation cron job:", error);
-  }
-});
+//     console.log("🎉 Monthly bills generation completed!");
+//   } catch (error) {
+//     console.error("❌ Error in bill generation cron job:", error);
+//   }
+// });
