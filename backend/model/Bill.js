77462bc6c858acb43e84ee6d3e28e88ccf6fc1ce
Your Name <your@email.com>
2025-08-25 11:21:52 +0300

    waxaan sameeyay feature cusub

--- backend/model/Bill.js
+++ backend/model/Bill.js
@@ -1,24 +1,9 @@
-import mongoose from "mongoose";
-
-const billSchema = new mongoose.Schema({
-  student: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Student",
-    required: true,
+const billSchema = new mongoose.Schema(
+  {
+    student: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
+    amount: { type: Number, required: true },
+    status: { type: String, enum: ["Unpaid", "Paid"], default: "Unpaid" },
+    lastPaidAt: { type: Date },
   },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["Unpaid", "Paid"],
-    default: "Unpaid",
-  },
-  lastPaidAt: {
-    type: Date, // goorta ugu dambeysay ee la "paid" dhahay
-  },
-});
-
-const Bill = mongoose.model("Bill", billSchema);
-export default Bill;
+  { timestamps: true } // <-- ku dar
+);
