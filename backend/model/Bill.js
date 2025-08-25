cae1e60c3bdbd92164d33ec2bccd2a58b5671157
Your Name <your@email.com>
2025-08-25 11:43:22 +0300

    waxaan sameeyay feature cusub

--- backend/model/Bill.js
+++ backend/model/Bill.js
@@ -1,7 +1,7 @@
 const billSchema = new mongoose.Schema(
   {
     student: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-    amount: { type: Number, required: true },
+
     status: { type: String, enum: ["Unpaid", "Paid"], default: "Unpaid" },
     lastPaidAt: { type: Date },
   },
