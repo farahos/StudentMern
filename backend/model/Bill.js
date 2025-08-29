6dd47a126a22623a28efbc5b75d72e3247471d3c
Farah <farah@example.com>
2025-08-29 07:30:32 +0300

    updated pro max

--- backend/model/Bill.js
+++ backend/model/Bill.js
@@ -1,10 +1,15 @@
+import mongoose from "mongoose";
+
 const billSchema = new mongoose.Schema(
   {
     student: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-
     status: { type: String, enum: ["Unpaid", "Paid"], default: "Unpaid" },
-     month: { type: String }, // YYYY-MM
+    month: { type: String }, // YYYY-MM
     lastPaidAt: { type: Date },
   },
-  { timestamps: true } // <-- ku dar
+  { timestamps: true } // si otomaatig ah u dar createdAt iyo updatedAt
 );
+
+const Bill = mongoose.model("Bill", billSchema);
+
+export default Bill;
